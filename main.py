b0f78335ea00df9d15a7c401285d311cf2995b68
Victor Drijkoningen <>
2024-04-26 11:11:06 +0200

    cleanup and nicer blinking

--- main.py
+++ main.py
@@ -11,13 +11,11 @@ with open(".env") as f:
 async def blink():
     led = machine.Pin(2, machine.Pin.OUT)
     while True:
-        led.value(1)
-        await asyncio.sleep(0.25)
         led.value(0)
+        await asyncio.sleep(0.25)
+        led.value(1)
         await asyncio.sleep(2)
     
-
-
 
 
 async def do_something_else():
@@ -26,7 +24,6 @@ async def do_something_else():
 async def main():
     await asyncio.gather(blink(), do_something_else())
 
-def start():
-    asyncio.run(main())
 
-start()
+asyncio.run(main())
+

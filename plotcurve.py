93834caa4074a44b2a2b490bd363102d23f17979
Victor Drijkoningen <>
2024-05-16 10:35:08 +0200

    last piece of curve return code

--- plotcurve.py
+++ plotcurve.py
@@ -6,14 +6,14 @@ import numpy
 
 
 def curve1(runtime):
-    time_section = [0,60, 90, 120, 300]
-    temp_section = [0,150, 200, 250, 0]
+    time_section = [0,60, 90, 120, 150, 300]
+    temp_section = [0,150, 200, 250, 250, 0]
 
     assert len(time_section) == len(temp_section)
 
     for i in range(len(time_section)):
         if runtime < time_section[i]:
-            return  
+            return  temp_section[i-1] + (temp_section[i] - temp_section[i-1])/(time_section[i]-time_section[i-1]) * (runtime-time_section[i-1])
 
 
 x = list()

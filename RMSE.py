3ec564c45bf152798bd518236e4831177e2489c9
Victor Drijkoningen <>
2024-06-06 16:21:08 +0200

    only high temps are important

--- RMSE.py
+++ RMSE.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def calc(filesfolder, datafilename):
     df = pd.read_csv(os.path.join(filesfolder,datafilename))
 
-    df = df.drop(df[df.target_temp < 25].index)
+    df = df.drop(df[df.target_temp < 110].index)
     df = df.drop('fan_duty', axis=1)
     df = df.drop('time', axis=1)
     df = df.drop('t2', axis=1)
